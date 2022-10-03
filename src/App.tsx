ec084007a63059b085026aca9b5bcec12d8f41f1
samdev <samuelberrus@gmail.com>
2022-10-04 00:32:08 +0200

    s2-16

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { Container } from "react-bootstrap";
 import Counter from "./bases/Counter";
 import CounterBy from "./bases/CounterBy";
 import CounterEffect from "./bases/CounterEffect";
 
 const App = () => {
 	return (
-		<>
+		<Container fluid>
 			<Counter />
 			<hr />
 			<CounterBy />
 			<hr />
 			<CounterEffect />
-		</>
+		</Container>
 	);
 };
 
--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+// imports
+import { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
 // constant
 const MAX_VALUE = 10;
 
@@ -6,10 +8,17 @@ const MAX_VALUE = 10;
 const CounterEffect = () => {
 	// states
 	const [counter, setCounter] = useState(8);
+	const counterHTML = useRef<HTMLHeadingElement>(null);
 
 	// methods
-	const handleClick = () => {
+	const handleClick = async () => {
 		if (counter >= MAX_VALUE) {
+			console.log("Se llego al máximo válido");
+
+			// gsap animation config
+			const tl = gsap.timeline();
+			tl.to(counterHTML.current, { y: -10, duration: 0.2, ease: "ease.out" });
+			tl.to(counterHTML.current, { y: 0, duration: 1, ease: "bounce.out" });
 			return;
 		}
 		setCounter((prev) => prev + 1);
@@ -23,7 +32,8 @@ const CounterEffect = () => {
 	//
 	return (
 		<>
-			<h1>Counter effect: {counter}</h1>
+			<h1>Counter effect: </h1>
+			<h2 ref={counterHTML}>{counter}</h2>
 			<button onClick={handleClick}>+1</button>
 		</>
 	);
